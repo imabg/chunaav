deb0d5e96f663121d02960c51c2fd8208c3400aa
Abhay Goswami <abhaygoswami676@gmail.com>
2020-04-13 19:22:38 +0530

    update route admin/login

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -7,8 +7,6 @@ const router = express.Router();
 const AdminController = require("../controller/AdminController");
 const VoterController = require("../controller/VoterController");
 
-router.post("/api/admin", AdminController.login);
-
 router.post("/api/admin/addVoter", auth, AdminController.addVoter);
 router.put("/api/admin/updateVoter", auth, AdminController.updateVoter);
 router.delete("/api/admin/deleteVoter", auth, AdminController.deleteVoter);
@@ -25,11 +23,14 @@ router.post("/api/login", VoterController.login);
 router.get("/api/voter/details", auth, VoterController.voterDetails);
 
 //Caste Vote
-router.post("/api/vote", auth, VoterController.vote)
+router.post("/api/vote", auth, VoterController.vote);
 
 // router.get("/api/admin/dashboard", auth, AdminController.dashboard);
 
 // ! Add admin API
 router.post("/api/admin/add", AdminController.add);
+
+// ADMIN LOGIN
+router.post("/api/admin/login", AdminController.login);
 
 module.exports = router;
