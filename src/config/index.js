ef597ac9030042cab4d7cfeceea0dd4cded03750
Abhay Goswami <abhaygoswami676@gmail.com>
2020-04-15 23:36:29 +0530

    add AWS credentials

--- src/config/index.js
+++ src/config/index.js
@@ -6,4 +6,8 @@ module.exports = {
   JWT_SECRET: process.env.JWT_SECRET,
   SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
   MAIL_ID: process.env.MAIL_ID,
+  Access_Key_ID: process.env.Access_Key_ID,
+  Secret_Access_Key: process.env.AWS_Secret_Access_Key,
+  CANDIDATES_BUCKET: process.env.AWS_CANDIDATES_BUCKET,
+  VOTERS_BUCKET: process.env.VOTERS_BUCKET,
 };
