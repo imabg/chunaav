0cb8af16a9c09dbec0c8b7675960fb94e16bcdc7
Abhay Goswami <abhaygoswami676@gmail.com>
2020-04-17 00:32:20 +0530

    static message template

---
+++ src/utils/MessageTemplates.js
@@ -0,0 +1,17 @@
+exports.NEW_VOTER = `
+Welcome to the Chunaav: The advanced and secure voting system
+You are successfully register for the election.
+#TeamChunaav
+`;
+
+exports.NEW_CANDIDATE = `
+Your name has been registered as a candidate in upcomming election.
+ Voters can vote you through by visiting booth or by Chunaav : The advanced and secure voting system
+ #TeamChunaav
+`;
+
+
+exports.SUCCESSFULL_VOTE=`
+Thank you for being a responsible citizen. Your vote has been recorded.
+#TeamChunaav
+`
