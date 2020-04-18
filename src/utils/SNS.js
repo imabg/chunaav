7b93429ecb06f848b503a705a8efa026a2b33f51
Abhay Goswami <abhaygoswami676@gmail.com>
2020-04-18 14:05:49 +0530

    update SNS

--- src/utils/SNS.js
+++ src/utils/SNS.js
@@ -1,19 +1,13 @@
 const AWS = require("aws-sdk");
 
-const { NEW_VOTER } = require("./MessageTemplates");
-
-module.exports = (phone_num) => {
-  var params = {
-    Message: NEW_VOTER,
-    PhoneNumber:  phone_num,
-  };
-
+module.exports = (phone_num, body) => {
+  const smsQuery = { PhoneNumber: phone_num, Message: body };
   var publishTextPromise = new AWS.SNS({
     accessKeyId: process.env.AWS_SNS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SNS_SECRET_ACCESS_KEY,
     region: process.env.AWS_SNS_REGION,
   })
-    .publish(params)
+    .publish(smsQuery)
     .promise();
 
   publishTextPromise
@@ -21,6 +15,7 @@ module.exports = (phone_num) => {
       return JSON.stringify({ MessageID: data.MessageId });
     })
     .catch(function (err) {
+      console.log(err);
       return JSON.stringify({ Error: err });
     });
 };
