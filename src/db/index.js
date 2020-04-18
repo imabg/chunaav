0897d415576e0eb26cca0735400f26837a17e1cf
Abhay Goswami <abhaygoswami676@gmail.com>
2020-04-19 01:35:59 +0530

    add mongodb atlas URL

--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 const config = require("../config");
 
-module.exports = mongoose.connect("mongodb://localhost:27017/chunav", {
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
+module.exports = mongoose.connect(
+  `mongodb+srv://abhay676:kYwI9sVPG2e16ktB@chunaav-j6xfh.mongodb.net/chunaav`,
+  {
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+  }
+);
