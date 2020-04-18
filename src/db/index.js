b696d0959404c4883f8e7ff1fcc3f1db000f4278
Abhay Goswami <abhaygoswami676@gmail.com>
2020-04-19 00:26:38 +0530

    fix: mongodb url typo

--- src/db/index.js
+++ src/db/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const config = require("../config");
 
-module.exports = mongoose.connect("mongodb:localhost:27017/chunav", {
+module.exports = mongoose.connect("mongodb://localhost:27017/chunav", {
   useCreateIndex: true,
   useUnifiedTopology: true,
   useNewUrlParser: true,
