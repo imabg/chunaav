a82f0a58a6ca833218037e82ac29b90e64f16917
Abhay Goswami <abhaygoswami676@gmail.com>
2020-04-18 14:07:00 +0530

    update message templates

--- src/utils/MessageTemplates.js
+++ src/utils/MessageTemplates.js
@@ -10,10 +10,11 @@ Your name has been registered as a candidate in upcomming election.
  #TeamChunaav
 `;
 
-
-exports.SUCCESSFULL_VOTE=`
+exports.SUCCESSFULL_VOTE = `
 Thank you for being a responsible citizen. Your vote has been recorded.
 #TeamChunaav
-`
+`;
 
-exports.OTPTemplate = `Your OTP for Chunaav:`
+exports.OTPTemplate = `Your OTP for Chunaav:`;
+
+exports.INFO = `Thanks for login @Chunaav. You have 5 minutes to cast your vote. After that you are not able to cast again. #TeamChunaav`;
